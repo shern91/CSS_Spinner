658397d8a752df4dfce1b0fa9da840f3cba05cda
yschin <ys.chin@softios.com>
2018-05-11 16:00:18 +0800

    Initial Commit & Finish CSS Spinners

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.main p').style.display = 'none';
+
+document.querySelector('.main').classList.add('spinner-1');
+
+setTimeout(() => {
+    document.querySelector('.main').classList.remove('spinner-1');
+    document.querySelector('.main p').style.display = 'block';
+}, 4000);
